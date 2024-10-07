e4a47409eb248d0afc5da0414a2adea291f40dec
eduardofsn <eduardonamiuti@hotmail.com>
2024-10-07 10:32:13 -0300

    primeiro e último commit

--- modelos/telefone.ts
+++ modelos/telefone.ts
@@ -1,4 +1,17 @@
-export default class Telefone {
+import Prototipo from "../interfaces/prototipo"
+
+export default class Telefone implements Prototipo {
     public ddd: string
     public numero: string
+
+
+    // Implementação do clone no telefone, assim como foi feito no endereço, como a atividade requisitou.
+
+    public clonar(): Prototipo {
+        let telefone = new Telefone()
+        telefone.ddd = this.ddd
+        telefone.numero = this.numero
+        return telefone
+    }
 }
+
